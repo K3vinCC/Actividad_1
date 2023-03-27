79956a69f84c5b7dd4ed6ff828ab1437b3a615fc
K3vin_CC <kcortes2021@alu.uct.cl>
2023-03-27 19:03:27 -0300

    update 0.2

--- taller_1/pages/index.js
+++ taller_1/pages/index.js
@@ -1,4 +1,34 @@
 
+// import { Inter } from 'next/font/google'
+
+// const inter = Inter({ subsets: ['latin'] })
+
+// export default function Home({ animeData }) {
+//   console.log(animeData)
+//     return (
+//       <div>
+//         <p>hola</p>
+//         <h1>{animeData.mal_id}</h1>
+//         <img src={animeData.image_url} alt={animeData.title} />
+//       </div>
+//     );
+// }
+// export async function getStaticProps() {
+//   const res = await fetch('https://api.jikan.moe/v4/anime/');
+//   const animeData = await res.json();
+
+//   return {
+//     props: {
+//       animeData
+//     }
+    
+//   }
+// }
+
+
+
+
+
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -7,14 +37,24 @@ export default function Home({ animeData }) {
   console.log(animeData)
     return (
       <div>
-        <p>hola</p>
-        <h1>{animeData.mal_id}</h1>
-        <img src={animeData.image_url} alt={animeData.title} />
+        {
+          animeData.data.map(gam => (
+            <div key={gam.mal_id}>
+
+              <h1>{gam.title}</h1>
+              <img src={gam.images.web} alt=""/>
+              <h3>Synopsis</h3>
+              <p>{gam.synopsis}</p>
+            </div>
+
+
+          ))
+        }
       </div>
     );
 }
 export async function getStaticProps() {
-  const res = await fetch('https://api.jikan.moe/v4/anime/');
+  const res = await fetch('https://api.jikan.moe/v4/anime');
   const animeData = await res.json();
 
   return {
